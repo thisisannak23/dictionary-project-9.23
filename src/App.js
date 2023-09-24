e5b621b0006183fc8c772169a259e7b5908c3d5b
thisisannak23 <anna.kazanecki@gmail.com>
2023-09-24 11:51:23 -0500

    title change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <img src {logo} className="App-logo img-fluid" alt="logo"/>
+      </header>
     </div>
   );
 }
